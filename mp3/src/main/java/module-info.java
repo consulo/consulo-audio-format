e41d14138c1a571dcf0de94aaca4b0f372d610e1
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-12-28 13:25:18 +0200

    init

---
+++ mp3/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * @author VISTALL
+ * @since 2025-12-28
+ */
+module consulo.audio.format.mp3 {
+    requires consulo.component.api;
+    
+    requires javazoom.jlayer;
+
+    requires javazoom.mp3;
+    
+    requires java.desktop;
+}
